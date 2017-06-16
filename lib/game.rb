74e1a7fbbd8cfd1df8babbff0e12aa6246c3a640
Filipe Fasolin <ffasolin@gmail.com>
2017-06-16 16:04:22 +0100

    Adds player turn to game start

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,5 @@
 class Game
   def start
-    'Choose a combination of A, B, C and 1, 2, 3'
+    ['Choose a combination of A, B, C and 1, 2, 3', 'X turn'].join("\n")
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,6 +3,12 @@ require_relative '../lib/game'
 describe Game do
   it 'starts a new game' do
     game = Game.new
-    expect(game.start).to eq 'Choose a combination of A, B, C and 1, 2, 3'
+    message = 'Choose a combination of A, B, C and 1, 2, 3'
+    expect(game.start).to include message
+  end
+
+  it 'new game contain player turn info' do
+    game = Game.new
+    expect(game.start).to include 'X turn'
   end
 end
